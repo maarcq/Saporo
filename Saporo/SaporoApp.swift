256913788c69218ee9a0ed27bd123b9b37776499
maarcq <marcellerq27@gmail.com>
2025-06-26 10:07:04 -0300

    Initial Commit

---
+++ Saporo/SaporoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaporoApp.swift
+//  Saporo
+//
+//  Created by Marcelle Ribeiro Queiroz on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SaporoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
