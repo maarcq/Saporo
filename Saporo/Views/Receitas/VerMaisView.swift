cb42d89b3ccbca4fa6305e3fda7468d9933ddb3f
bernardosmm <bernardosmm2004@gmail.com>
2025-07-01 11:18:46 -0300

    fix: resolução de conflitos

--- Saporo/Views/Receitas/VerMaisView.swift
+++ Saporo/Views/Receitas/VerMaisView.swift
@@ -36,13 +36,14 @@ struct VerMaisView: View {
     }
 }
 
-#Preview {
-    VerMaisView(text: "", receitas: [
-        Recipe(id: 1, title: "Bolo de Cenoura", image: "ImageTest", imageType: nil, readyInMinutes: 45, servings: 8),
-        Recipe(id: 2, title: "Pizza Margherita", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2),
-        Recipe(id: 23, title: "Pizza Margherita1", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2),
-        Recipe(id: 24, title: "Pizza Margherita2", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2),
-        Recipe(id: 25, title: "Pizza Margherita3", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2),
-        Recipe(id: 26, title: "Pizza Margherita4", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2)
-    ])
-}
+//#Preview {
+//    VerMaisView(text: "", receitas: [
+//        Recipe(id: 1, title: "Bolo de Cenoura", image: "ImageTest", imageType: nil, readyInMinutes: 45, servings: 8),
+//        Recipe(id: 2, title: "Pizza Margherita", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2),
+//        Recipe(id: 23, title: "Pizza Margherita1", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2),
+//        Recipe(id: 24, title: "Pizza Margherita2", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2),
+//        Recipe(id: 25, title: "Pizza Margherita3", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2),
+//        Recipe(id: 26, title: "Pizza Margherita4", image: "ImageTest", imageType: nil, readyInMinutes: 20, servings: 2)
+//    ])
+//}
+
